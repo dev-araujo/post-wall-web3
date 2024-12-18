d79d7aa0d8c90469e7d02a302aa2a634c9a3a858
dev-araujo <theriancirculo@protonmail.com>
2024-12-18 18:33:47 -0300

    feat: contract

---
+++ dapp/script/PostWall.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {PostWall} from "../src/PostWall.sol";
+
+contract CounterScript is Script {
+    PostWall public postWall;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        postWall = new PostWall();
+
+        vm.stopBroadcast();
+    }
+}
---
+++ dapp/src/PostWall.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract PostWall {
+    struct Message {
+        address sender;
+        string text;
+        uint256 timestamp;
+    }
+
+    Message[] public messages;
+
+    event NewMessage(address indexed sender, string text, uint256 timestamp);
+
+    function sendMessage(string memory _text) public {
+        require(bytes(_text).length > 0, "Message cannot be empty");
+
+        Message memory newMessage = Message(msg.sender, _text, block.timestamp);
+        messages.push(newMessage);
+
+        emit NewMessage(msg.sender, _text, block.timestamp);
+    }
+
+    function getMessages() public view returns (Message[] memory) {
+        return messages;
+    }
+}
